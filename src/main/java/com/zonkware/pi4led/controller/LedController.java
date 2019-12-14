19bdf19efb4b6d5c637cb838dd70ffffbf5c9a70
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2019-12-14 17:10:38 +0100

    Adjust project with button

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -1,13 +1,33 @@
 package com.zonkware.pi4led.controller;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LedController {
 
-    private static GpioPinDigitalOutput pin;
+    private final GpioController gpio = GpioFactory.getInstance();
+    private final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+    private final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+
+    public LedController() {
+        initialize();
+    }
+
+    public void initialize(){
+        myButton.setShutdownOptions(true);
+        pin.setShutdownOptions(true, PinState.LOW);
+
+        myButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                pin.toggle();
+            }
+        });
+    }
 
     @RequestMapping("/")
     public String greeting() {
@@ -16,13 +36,7 @@ public class LedController {
 
     @RequestMapping("/light")
     public String light() {
-        if (pin == null) {
-            GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
-        }
-
         pin.toggle();
-
         return "OK";
     }
 
