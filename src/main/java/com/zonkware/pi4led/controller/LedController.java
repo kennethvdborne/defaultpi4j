bc919c0acedd5d77434ff4be39d92839c18687f5
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-02-21 15:40:14 +0100

    Some more testing

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LedController {
 
     private final GpioController gpio = GpioFactory.getInstance();
-    private final GpioPinDigitalInput myButton1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-    private final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+   // private final GpioPinDigitalInput myButton1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+   // private final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
     private final GpioPinDigitalInput myButton2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, PinPullResistance.PULL_DOWN);
     private final GpioPinDigitalOutput led2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.LOW);
 
@@ -20,24 +20,15 @@ public class LedController {
     }
 
     public void initialize(){
-        myButton1.setShutdownOptions(true);
-        led1.setShutdownOptions(true, PinState.LOW);
+
         myButton2.setShutdownOptions(true);
         led2.setShutdownOptions(true, PinState.LOW);
-        myButton1.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                if (event.getState().isLow()){
-                    led1.toggle();
-                }
-                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            }
-        });
+
         myButton2.addListener(new GpioPinListenerDigital() {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                 try {
-                    onOff(led1, led2);
+                    onOff(led2);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -53,23 +44,19 @@ public class LedController {
     private void startup() throws InterruptedException {
         for (int i = 0; i < 3; i++) {
             led2.toggle();
-            led1.toggle();
             Thread.sleep(400);
         }
         for (int i = 0; i < 8; i++) {
             led2.toggle();
-            led1.toggle();
             Thread.sleep(100);
         }
     }
 
-    public void onOff(GpioPinDigitalOutput l1, GpioPinDigitalOutput l2) throws InterruptedException {
+    public void onOff(GpioPinDigitalOutput l1) throws InterruptedException {
         System.out.println("Looping started");
         for (int i = 0; i < 4; i++) {
             l1.toggle();
             Thread.sleep(100);
-            l2.toggle();
-            Thread.sleep(300);
         }
         System.out.println("Looping Ended");
     }
@@ -81,7 +68,6 @@ public class LedController {
 
     @RequestMapping("/light")
     public String light() {
-        led1.toggle();
         return "OK";
     }
 
